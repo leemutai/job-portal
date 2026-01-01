14e5eb82324bd57ccf677376ae3f0edfa4d488ff
leemutai <mutailee361@gmail.com>
2026-01-01 23:35:47 +0300

    feat: setup frontend routing and core dependencies, Installed frontend dependencies: react-router-dom (routing), react-toastify (notifications), quill (rich text editor) Added initial pages (Home, ApplyJob, Applications), Configured routing in main.jsx using BrowserRouter (removed StrictMode), Defined application routes in App.jsx with respective paths, Project structure prepared for further feature development,

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import ApplyJob from './pages/ApplyJob'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/apply-job:id' element={<ApplyJob/>} />
+        <Route path='/apply-job:id' element={<ApplyJob/>} />
+      </Routes>
+      </div>
   )
 }
 
