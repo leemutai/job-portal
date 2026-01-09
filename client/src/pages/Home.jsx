b92209763225aff1933a7d6820dd3d7ed19cfbe5
leemutai <mutailee361@gmail.com>
2026-01-09 23:34:24 +0300

    added app download CTA section and global footer components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import JobListings from '../components/JobListings'
+import AppDownload from '../components/AppDownload'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Navbar/>
       <Hero/>
       <JobListings/>
+      <AppDownload/>
+      <Footer/>
     </div>
   )
 }
