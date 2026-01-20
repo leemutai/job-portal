8ab431cdea3b4efcee9a1025343a2aca1ebf9247
leemutai <mutailee361@gmail.com>
2026-01-20 20:52:33 +0300

    aligned ApplyJob sidebar and correct flex width distribution -Removed extra wrapper causing width constraints - Properly applied lg:w-2/3 and lg:w-1/3 layout - Improved spacing and alignment for right-side job cards

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const Applications = () => {
   return (
-    <div>Applications</div>
+    <>
+    <Navbar />
+
+    </>
   )
 }
 
