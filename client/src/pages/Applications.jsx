c4beb6246c45da119d932409e35009d78c2f4069
leemutai <mutailee361@gmail.com>
2026-01-26 21:57:28 +0300

    added resume view and edit state handling

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 
 const Applications = () => {
+
+  const [isEdit , setIsEdit] = useState(false)
+
+
   return (
     <>
     <Navbar />
+
+    <div className='container px-4 min-h-[65vh] 2xl:px-20 mx-auto my-10'>
+      <h2 className='text-xl font-semibold'>Your Resume</h2>
+      <div className='flex gap-2 mb-6 mt-3'>
+        {
+          isEdit 
+          ?  <>
+
+          </>
+          : <div className='flex gap-2'>
+            <a className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg' href="">
+              Resume
+            </a>
+            <button onClick={() => setIsEdit(true)} className='text-gray-500 border border-gray-300 rounded-lg px-4 py-2'>
+              Edit
+            </button>
+          </div>
+        }
+
+      </div>
+    </div>
 
     </>
   )
