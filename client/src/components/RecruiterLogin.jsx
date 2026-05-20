fd421bdbed7a37cc81aa0f7bb57c5a016efbffd3
leemutai <mutailee361@gmail.com>
2026-05-20 20:29:10 +0300

    feat: add recruiter login modal UI and form logic

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const RecruiterLogin = () => {
 
@@ -14,8 +15,43 @@ const RecruiterLogin = () => {
 
   return (
     
-    <div> 
-        Login Popup
+    <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'> 
+       
+       <form  className='relative bg-white p-10 rounded-xl text-slate-500'>
+        <h1 className='text-center text-2xl text-neutral-700 font-medium'>Recruiter {state} </h1>
+        <p className='text-sm'>Welcome back! Please sign in to continue</p>
+        <>
+
+        {state !== 'Login' && (
+          <div className='border px-4 flex items-center gap-2 rounded-full mt-5'>
+          <img src={assets.person_icon} alt="" />
+          <input className='outline-none text-sm' onChange={e=> setName(e.target.value)} value={name} type="text" placeholder='Company Name' required />
+        </div>
+
+        )}
+
+        
+
+         <div className='border px-4 flex items-center gap-2 rounded-full mt-5'>
+          <img src={assets.email_icon} alt="" />
+          <input className='outline-none text-sm' onChange={e=> setEmail(e.target.value)} value={email} type="email" placeholder='Email Id' required />
+        </div>
+
+         <div className='border px-4 flex items-center gap-2 rounded-full mt-5'>
+          <img src={assets.lock_icon} alt="" />
+          <input className='outline-none text-sm' onChange={e=> setPassword(e.target.value)} value={password} type="password" placeholder='Password' required />
+        </div>
+
+        <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot password?</p>
+
+        </>
+
+        <button className='bg-blue-600 w-full text-white py-2 rounded-full'>
+          {state === 'Login' ? 'login' : 'create account'}
+
+        </button>
+
+       </form>
 
     </div>
   )
