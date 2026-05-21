e30eab603080f2cf10d4f84f011cac11aeab129c
leemutai <mutailee361@gmail.com>
2026-05-21 22:44:46 +0300

    feat: add dashboard navbar with recruiter profile hover dropdown

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { assets } from '../assets/assets'
 
 const Dashboard = () => {
   return (
-    <div>
-        Dashboard
-        <Outlet />
+    <div className='min-h-screen'>
+
+        { /* Navbar for rectruiter panel */}
+        <div className='shadow py-4'>
+            <div className='px-5 flex justify-between items-center'>
+                <img className='max-sm:w-32 cursor-pointer' src={assets.logo} alt="" />
+                <div className='flex items-center gap-3'>
+                    <p className='max-sm:hidden'>Welcome, LeeTech Solutions</p>
+                    <div className='relative group'>
+                        <img className='w-8 border rounded-full' src={assets.company_icon} alt="" />
+                        <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black  rounded pt-12  '>
+                            <ul>
+                                <li>Logout</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     </div>
   )
 }
