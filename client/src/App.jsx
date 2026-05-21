1d2330ca2fbedd07f83918fd3315275c15ed4a16
leemutai <mutailee361@gmail.com>
2026-05-21 18:51:51 +0300

    added 4 new pages and feat: implement dashboard layout with nested routing using Outlet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,10 @@ import Applications from './pages/Applications'
 import { UserProfile } from '@clerk/clerk-react'
 import RecruiterLogin from './components/RecruiterLogin'
 import { AppContext } from './context/AppContext'
+import Dashboard from './pages/Dashboard'
+import AddJob from './pages/AddJob'
+import ManageJobs from './pages/ManageJobs'
+import ViewApplications from './pages/ViewApplications'
 
 const App = () => {
 
@@ -19,6 +23,12 @@ const App = () => {
         <Route path='/' element={<Home/>} />
         <Route path='/apply-job/:id' element={<ApplyJob/>} />
         <Route path='/applications' element={<Applications/>} />
+        <Route path='/dashboard' element={<Dashboard/>}>  
+
+        <Route path='add-job' element={<AddJob/>} />
+        <Route path='manage-jobs' element={<ManageJobs/>} />
+        <Route path='view-applications' element={<ViewApplications/>} />
+        </Route>
         {/* Clerk profile & settings */}
       <Route
         path='/user-profile/*'
