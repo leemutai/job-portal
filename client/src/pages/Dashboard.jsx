c8dd130629bd50757a453ab2887a7621c5471951
leemutai <mutailee361@gmail.com>
2026-05-23 12:37:55 +0300

    fix: correct dashboard sidebar routing and active NavLink behavior Fixed NavLink active state leaking from /dashboard to nested routes, Added proper route structure for add-job, manage-jobs, and view-applications, Updated sidebar navigation to use full nested paths, Improved UX by ensuring independent active highlighting per route

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
+import { NavLink, Outlet, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
 
 const Dashboard = () => {
@@ -56,11 +56,35 @@ const Dashboard = () => {
 
         </div>
       </div>
+      <div className='flex items-start'>
 
-      {/* Dashboard Nested Pages */}
-      <div className='p-5'>
-        <Outlet />
+        {/*Left  Sidebar with options to add job, manage jobs, view applications */}
+        <div className='inline-block min-h-screen border-r-2'>
+          <ul className='flex flex-col items-start pt-5 text-gray-800'>
+            <NavLink className={({isActive}) => ` flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`}  to={'/dashboard/add-job'}>
+            <img src={assets.add_icon} alt="" />
+            <p>Add Job</p>
+            </NavLink>
+
+            <NavLink className={({isActive}) => ` flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`}  to={'/dashboard/manage-jobs'}>
+            <img src={assets.home_icon} alt="" />
+            <p>Manage Jobs</p>
+            </NavLink>
+
+            <NavLink className={({isActive}) => ` flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`}  to={'/dashboard/view-applications'}>
+            <img src={assets.person_tick_icon} alt="" />
+            <p>View Applications</p>
+            </NavLink>
+          </ul>
+        </div>
+
+        <div>
+          <Outlet />
+        </div>
+
       </div>
+
+    
 
     </div>
   )
