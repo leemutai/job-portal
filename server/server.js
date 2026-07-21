04980cf5121bcec00fa6d2ff09e5e05719ec7713
leemutai <mutailee361@gmail.com>
2026-07-21 23:35:06 +0300

    feat: added Vercel deployment config and webhook route - Add vercel.json for deploying Express server as serverless function - Route all requests through server.js via @vercel/node runtime - Wire up POST /webhooks route to clerkWebhooks controller

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/db.js';
 import * as Sentry from "@sentry/node";
+import { clerkWebhooks } from './controllers/webhooks.js';
 
 
 // Initialize express app
@@ -22,6 +23,8 @@ app.get('/',(req, res) => res.send("API is running..."));
 app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
+
+app.post('/webhooks', clerkWebhooks)
 
 // PORT
 
