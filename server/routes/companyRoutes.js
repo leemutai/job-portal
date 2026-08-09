5fba98ff108d027f5327942ec651376266c149b9
leemutai <mutailee361@gmail.com>
2026-08-09 20:36:19 +0300

    feat: added company recruiter API routes

---
+++ server/routes/companyRoutes.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { changeJobApplicationStatus, changeVisibility, getCompanyData, getCompanyJobApplicants, getCompanyPostedJobs, loginCompany, postJob, registerCompany } from "../controllers/companyController.js";
+
+const router = express.Router();
+
+// Register a company
+router.post('/register', registerCompany)
+
+// Company login
+router.post('/login', loginCompany)
+
+// get company data
+router.get('/company', getCompanyData)
+
+// Post a job
+router.post('/post-job', postJob)
+
+// Get Applicants data of Company
+router.get('/applicants', getCompanyJobApplicants)
+
+// Get company job lists
+router.get('/list-jobs', getCompanyPostedJobs)
+
+//Change application status
+router.post('/change-status', changeJobApplicationStatus)
+
+// Change Applications Visibility
+router.post('/change-visibility', changeVisibility)
+
+export default router;
