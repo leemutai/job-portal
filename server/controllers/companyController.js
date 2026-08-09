7b83cb3a5ef37401e7b3d18793fc286804ba8c5c
leemutai <mutailee361@gmail.com>
2026-08-09 20:20:59 +0300

    feat: added company and recruiter controller functions

---
+++ server/controllers/companyController.js
@@ -0,0 +1,40 @@
+
+// Register a new company
+export const registerCompany = async (req, res) => {
+
+}
+
+// Company Login
+export const loginCompany = async (req, res) => {
+
+}
+
+//Get company data
+export const getCompanyData = (req, res) => {
+
+}
+
+// Post a new job
+export const postJob = async (req, res) => {
+
+}
+
+// Get company job applicants
+export const getCompanyJobApplicants = async (req, res) => {
+
+}
+
+// Get company posted jobs
+export const getCompanyPostedJobs = async (req, res) => {
+
+}
+
+// change job application status
+export const changeJobApplicationStatus = async (req, res) => {
+
+}
+
+// change job visibility status
+export const changeVisibility = async (req, res) => {
+    
+}
