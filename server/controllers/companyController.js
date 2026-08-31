ae075e4c778008a4660e74878b38029f55555141
leemutai <mutailee361@gmail.com>
2026-08-31 21:05:21 +0300

    feat: completed company registration with image upload

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -23,6 +23,8 @@ export const registerCompany = async (req, res) => {
 
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
+
+        
         
     } catch (error) {
         
