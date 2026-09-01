d33f7cf36072575d770e29e628e6168ed1352ad4
leemutai <mutailee361@gmail.com>
2026-09-01 20:41:24 +0300

    fix(company): added logging and proper status codes to registerCompany

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -1,5 +1,7 @@
 import Company from "../models/Company.js";
 import bcrypt from "bcrypt";
+import { v2 as cloudinary } from "cloudinary";
+import generateToken from "../utils/generateToken.js";
 
 // Register a new company
 export const registerCompany = async (req, res) => {
@@ -24,9 +26,31 @@ export const registerCompany = async (req, res) => {
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path)
+
+        const company = await Company.create({
+            name,
+            email,
+            password: hashedPassword,
+            image: imageUpload.secure_url
+        })
+
+        res.json({
+            success: true,
+            company: {
+                _id: company._id,
+                name: company.name,
+                email: company.email,
+                image: company.image
+            },
+            token: generateToken(company._id)
+
+        })
+
         
         
     } catch (error) {
+        res.json({success:false, message: error.message})
         
     }
 
