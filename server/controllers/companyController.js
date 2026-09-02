f607a5c0f295475c6a63ae62915d83de13da2889
leemutai <mutailee361@gmail.com>
2026-09-02 21:32:22 +0300

    feat: implemented job posting

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -2,6 +2,7 @@ import Company from "../models/Company.js";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import generateToken from "../utils/generateToken.js";
+import Job from "../models/job.js";
 
 // Register a new company
 export const registerCompany = async (req, res) => {
@@ -101,11 +102,32 @@ export const getCompanyData = (req, res) => {
 // Post a new job
 export const postJob = async (req, res) => {
 
-    const {title, description, location, salary} = req.body
+    const {title, description, location, salary, level, category} = req.body
 
      const companyId = req.company._id
 
-     console.log(companyId, {title, description, location, salary})
+    try {
+
+        const newJob = new Job({
+            title,
+            description,
+            location,
+            salary,
+            companyId,
+            date: Date.now(),
+            level,
+            category
+        })
+
+        await newJob.save()
+
+        res.json({success: true, newJob})
+        
+    } catch (error) {
+
+        res.json({success: false, message: error.message})
+        
+    }
 
 }
 
