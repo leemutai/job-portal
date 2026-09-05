18fe76ad4ba264092fb87dbc77a87693e683e7f6
leemutai <mutailee361@gmail.com>
2026-09-05 23:18:00 +0300

    feat: implemented get user data with Clerk auth

---
+++ server/controllers/userController.js
@@ -0,0 +1,42 @@
+import User from "../models/User.js";
+
+
+// Get user Data
+export const getUserData = async (req, res) => {
+
+    const userId = req.auth.userId;
+
+    try {
+
+        const user = await User.findById(userId)
+
+        if (!user) {
+
+            return res.json({success: false, message: "User not found"})
+            
+        }
+
+        res.json({success: true, user})
+        
+    } catch (error) {
+
+        res.json({success: false, message: error.message})
+        
+    }
+}
+
+// Apply for a job
+export const applyForJob = async (req, res) => {
+
+}
+
+
+// Get user applied applications
+export const getUserJobApplications = async (req, res) => {
+
+}
+
+// Update user profile (resume)
+export const updateUserResume = async (req, res) => {
+    
+}
