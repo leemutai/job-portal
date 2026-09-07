12ef1912e3825e10152e824bd874cd135d4e2865
leemutai <mutailee361@gmail.com>
2026-09-07 20:30:06 +0300

    feat: added job application model

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ await connectCloudinary();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware())
 
 // Routes
 app.get('/',(req, res) => res.send("API is running..."));
