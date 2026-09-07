d49f8b3289470f261c8d1ab13c0d70c731215ad9
leemutai <mutailee361@gmail.com>
2026-09-07 20:57:38 +0300

    feat: added resume upload and profile update controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import Job from "../models/job.js";
 import JobApplication from "../models/JobApplication.js";
 import User from "../models/User.js";
+import { v2 as cloudinary } from 'cloudinary';
 
 
 // Get user Data
@@ -100,5 +101,30 @@ export const getUserJobApplications = async (req, res) => {
 
 // Update user profile (resume)
 export const updateUserResume = async (req, res) => {
+
+    try {
+
+        const userId = req.auth.userId
+
+        const resumeFile = req.resumeFile
+
+        const userData = await User.findById(userId)
+
+        if (resumeFile) {
+
+            const resumeUpload = await cloudinary.uploader.upload(resumeFile.path)
+            userData.resume = resumeUpload.secure_url
+            
+        }
+
+        await userData.save()
+
+        return res.json({success: true, message: 'Resume updated '})
+        
+    } catch (error) {
+
+        res.json({success: false, message: error.message})
+        
+    }
     
 }
