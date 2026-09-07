bc198ffa68d308b8960e3427f652832985f97123
leemutai <mutailee361@gmail.com>
2026-09-07 20:39:58 +0300

    feat: added job application controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,5 @@
+import Job from "../models/job.js";
+import JobApplication from "../models/JobApplication.js";
 import User from "../models/User.js";
 
 
@@ -27,6 +29,43 @@ export const getUserData = async (req, res) => {
 
 // Apply for a job
 export const applyForJob = async (req, res) => {
+
+    const {jobId} = req.body;
+
+    const userId = req.auth.userId;
+
+    try {
+
+        const isAlreadyApplied = await JobApplication.find({jobId, userId})
+
+        if (isAlreadyApplied.length > 0) {
+
+            return res.json({success: false, message: 'Already applied'})
+            
+        }
+
+        const jobData = await Job.findById(jobId)
+
+        if (!jobData) {
+
+            return res.json({success: false, message: 'Job not found'})
+            
+        }
+
+        await JobApplication.create({
+            companyId: jobData.companyId,
+            userId,
+            jobId,
+            date: Date.now()
+        })
+        
+
+        res.json({success: true, message: 'Applied successfully'})
+    } catch (error) {
+
+        res.json({success: false, message: error.message})
+        
+    }
 
 }
 
